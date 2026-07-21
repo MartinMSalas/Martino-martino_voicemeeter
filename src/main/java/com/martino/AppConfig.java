0321ac0566e66272b7338bec48b7d2b2547222d1
MartinMSalas <ingmartinmsalas@gmail.com>
2026-07-21 11:37:46 -0300

    fix<AppConfig>:step 0.5db

--- src/main/java/com/martino/AppConfig.java
+++ src/main/java/com/martino/AppConfig.java
@@ -8,7 +8,7 @@ public final class AppConfig {
     public static final String CAMILLA_DSP_WS_URL =
             "ws://127.0.0.1:1234";
 
-    public static final float VOLUME_STEP_DB = 1.0f;
+    public static final float VOLUME_STEP_DB = 0.5f;
 
     public static final float MIN_GAIN_DB = -60.0f;
     public static final float MAX_GAIN_DB = 0.0f;
